cd87c2a9273175f9e21947007b42874843f1efaa
metakata <menning23@gmail.com>
2016-04-10 16:20:52 -0400

    installing and using requests module

---
+++ Level5/List_of_Dictionaries.py
@@ -0,0 +1,23 @@
+first_item = {
+				'name': 'Spam n Eggs',
+				'description': 'Two eggs with Spam',
+				'price': 2.50
+			}
+
+second_item = {
+				'name': 'Spam n Jam',
+				'description': 'Buscuit with Jam with Spam',
+				'price': 3.50
+			}
+			
+second_item = {
+				'name': 'Spam n Ham',
+				'description': 'Ham & Spam',
+				'price': 4.50
+			}
+			
+menu_items = [first_item, second_item, third_item]
+
+print(menu_items[0]['name'], menu_items[0]['price'], menu_items[0]['desc'])
+print(menu_items[1]['name'], menu_items[1]['price'], menu_items[1]['desc'])
+print(menu_items[2]['name'], menu_items[2]['price'], menu_items[2]['desc'])
