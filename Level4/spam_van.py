164a204a0e7ad9ab17c2221887267523ce0453dd
metakata <menning23@gmail.com>
2016-04-10 15:43:36 -0400

    reading files

--- Level4/spam_van.py
+++ Level4/spam_van.py
@@ -1,3 +1,14 @@
+def read_dollar_menu():
+	dollar_spam = open('dollar_menu.txt', 'r')
+	
+	dollar_menu = []
+	for line in dollar_spam:
+		line = line.strip()
+		dollar_menu.append(line)
+	
+	print(dollar_menu)
+	dollar_spam.close()
+
 def print_menu(menu):
 	for name, price in menu.items():
 		print(name, ': $', format(price, '.2f'), sep='')
@@ -42,6 +53,7 @@ def write_sales_log(order):
 	file.close()
 			
 def main():
+	read_dollar_menu()
 	menu = {'Knackered Spam': 4.0, 'Pip pip Spam': 3.0, 'Cheeky Spam':1.0, 'Cheerio Spam': 2.0}
 	print_menu(menu)
 	orders = get_order(menu)
